1def3a6b80a41e779314c8ff9d0f6dc565f129cc
Adrien Tremblay <adrien@adrientremblay.com>
2020-07-05 21:12:47 -0400

    Create Blog component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from "react";
 import "./App.css";
+import Blog from "./containers/Blog/Blog";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Blog />
+    </div>
+  );
 }
 
 export default App;
