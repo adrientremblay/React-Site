a6f154b98687eb6c7790db2383a03ae2132a545b
Adrien Tremblay <adrien@adrientremblay.com>
2020-07-06 18:02:16 -0400

    Create models for user and post

---
+++ backend/models/post.model.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const postSchema = new Schema(
+  {
+    username: { type: String, required: true },
+    body: { type: String, required: true },
+    date: { type: DataCue, required: true },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Post = mongoose.model({"Post", postSchema});
+
+module.exports = Post;
