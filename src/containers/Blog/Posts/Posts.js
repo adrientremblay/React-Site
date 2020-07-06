4ee30f0c4b7fa2e3a1ac7a60588720a4c120929e
Adrien Tremblay <adrien@adrientremblay.com>
2020-07-06 15:06:17 -0400

    More styling

--- src/containers/Blog/Posts/Posts.js
+++ src/containers/Blog/Posts/Posts.js
@@ -10,7 +10,11 @@ const posts = (props) => {
         desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Massa eget egestas purus viverra accumsan in nisl nisi scelerisque. Ac turpis egestas sed tempus urna et pharetra. Metus aliquam eleifend mi in nulla posuere sollicitudin aliquam. Nunc mattis enim ut tellus elementum sagittis. Id consectetur purus ut faucibus pulvinar elementum. Id venenatis a condimentum vitae sapien pellentesque. Pretium vulputate sapien nec sagittis. Lorem donec massa sapien faucibus et. Nec feugiat in fermentum posuere urna nec tincidunt. Aenean sed adipiscing diam donec adipiscing tristique risus."
       ></Post>
 
-      <Post title="sample title" date="12 aug 2069" desc="stuff"></Post>
+      <Post
+        title="sample title"
+        date="12 aug 2069"
+        desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Massa eget egestas purus viverra accumsan in nisl nisi scelerisque. Ac turpis egestas sed tempus urna et pharetra. Metus aliquam eleifend mi in nulla posuere sollicitudin aliquam. Nunc mattis enim ut tellus elementum sagittis. Id consectetur purus ut faucibus pulvinar elementum. Id venenatis a condimentum vitae sapien pellentesque. Pretium vulputate sapien nec sagittis. Lorem donec massa sapien faucibus et. Nec feugiat in fermentum posuere urna nec tincidunt. Aenean sed adipiscing diam donec adipiscing tristique risus."
+      ></Post>
     </React.Fragment>
   );
 };
