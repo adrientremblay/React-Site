d145cf6b82338b408ea35f545d5844d32d863aba
Adrien Tremblay <adrien@adrientremblay.com>
2020-07-07 15:07:46 -0400

    Add more routes for post API

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -24,4 +24,31 @@ router.route("/add").post((req, res) => {
     .catch((err) => res.status(400).json("Error: " + err));
 });
 
+router.route("/:id").get((req, res) => {
+  Post.findById(req.params.id)
+    .then((post) => res.json(post))
+    .catch((err) => res.status(400).json("Error: " + err));
+});
+
+router.route("/:id").delete((req, res) => {
+  Post.findByIdAndDelete(req.params.id)
+    .then(() => res.json("Post deleted."))
+    .catch((err) => res.status(400).json("Error: " + err));
+});
+
+router.route("/update/:id").post((req, res) => {
+  Post.findById(req.params.id)
+    .then((post) => {
+      post.username = req.body.username;
+      post.description = req.body.description;
+      post.date = Date.parse(req.body.date);
+
+      post
+        .save()
+        .then(() => res.json("Post Updated"))
+        .catch((err) => res.status(400).json("Error: " + err));
+    })
+    .catch((err) => res.status(400).json("Error: " + err));
+});
+
 module.exports = router;
