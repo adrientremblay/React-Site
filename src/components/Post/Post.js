9f71fb4bd58af395b496e7c154bd3c4b1c4873bd
Adrien Tremblay <adrien@adrientremblay.com>
2020-07-10 22:22:58 -0400

    Make blog posts draggable

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -1,19 +1,22 @@
 import React from "react";
 import classes from "./Post.module.css";
 import TerminalBar from "./TerminalBar/TerminalBar";
+import Draggable from "react-draggable";
 
 const post = (props) => {
   return (
-    <div className={classes.Post}>
-      <TerminalBar username={props.author} />
-      <span className={classes.Title}>{props.title}</span>
-      <div className={classes.info}>
-        <ul>
-          <li>Date: {props.date}</li>
-          <li>Description: {props.desc}</li>
-        </ul>
+    <Draggable>
+      <div className={classes.Post}>
+        <TerminalBar username={props.author} />
+        <span className={classes.Title}>{props.title}</span>
+        <div className={classes.info}>
+          <ul>
+            <li>Date: {props.date}</li>
+            <li>Description: {props.desc}...</li>
+          </ul>
+        </div>
       </div>
-    </div>
+    </Draggable>
   );
 };
 
