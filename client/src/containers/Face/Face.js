6d62ce9466a0df9291e9cecb9eb98a930cebf64c
Adrien Tremblay <adrien@adrientremblay.com>
2020-08-01 16:50:18 -0400

    Basic typist and install pkgs

--- client/src/containers/Face/Face.js
+++ client/src/containers/Face/Face.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
-// import classes from "./Face.module.css";
+import Typist from "react-typist";
+import "react-typist/dist/Typist.css";
+import classes from "./Face.module.css";
 import IconButton from "../../components/IconButton/IconButton";
 import Title from "../../components/Title/Title";
 
@@ -7,27 +9,26 @@ class Face extends Component {
   render() {
     return (
       <div>
-        <Title>Hi i'm Adrien</Title>
-        <p>
+        <Typist>
+          <Title>Hi i'm Adrien</Title>
           I am a Software Engineering student currently studying at Concordia
           University, Montreal, Canada
-        </p>
-        <p>
+          <br />
+          <br />
           My interests range from web development, to game development, to
-          operating systems, to anything computer science.
-        </p>
-        <p>I'm also an avid runner, reader, and singer.</p>
-        <p>See my</p>
-
-        <IconButton
-          href="https://github.com/adrientremblay"
-          icon={["fab", "github"]}
-        />
-        <IconButton
-          href="https://www.linkedin.com/in/adrientremblay/"
-          icon={["fab", "linkedin"]}
-        />
-        <IconButton href="mailto:adrien@adrientremblay.com" icon="at" />
+          operating systems, to anything computer science. I'm also an avid
+          runner, reader, and singer. See my
+          <hr />
+          <IconButton
+            href="https://github.com/adrientremblay"
+            icon={["fab", "github"]}
+          />
+          <IconButton
+            href="https://www.linkedin.com/in/adrientremblay/"
+            icon={["fab", "linkedin"]}
+          />
+          <IconButton href="mailto:adrien@adrientremblay.com" icon="at" />
+        </Typist>
       </div>
     );
   }
