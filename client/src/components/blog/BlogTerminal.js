3a37e73fda15eccf879fc76d3137e53d5de7476f
Adrien Tremblay <adrien@adrientremblay.com>
2020-08-03 17:24:37 -0400

    List, Open, and display HTML blog posts

--- client/src/components/blog/BlogTerminal.js
+++ client/src/components/blog/BlogTerminal.js
@@ -1,11 +1,20 @@
 import React, { Component } from "react";
 import Terminal from "react-console-emulator";
 import axios from "axios";
+import draftToHtml from "draftjs-to-html";
 
 class BlogTerminal extends Component {
   constructor(props) {
     super(props);
+    this.state = { posts: [] };
     this.terminal = React.createRef();
+  }
+
+  componentDidMount() {
+    axios.get("/api/posts").then((request) => {
+      const posts = request.data;
+      this.setState({ posts: posts });
+    });
   }
 
   commands = {
@@ -17,17 +26,32 @@ class BlogTerminal extends Component {
       },
     },
     ls: {
-      description: "List all posts",
+      description: "List all posts.",
       usage: "ls",
       fn: () => {
+        // TODO: get rid of this terminal crap and just use '/n's
         const terminal = this.terminal.current;
-        axios.get("/api/posts").then((request) => {
-          const posts = request.data;
-          terminal.pushToStdout("foo");
-          terminal.pushToStdout("bar");
+        let { posts } = this.state;
+        let count = 0;
+        posts.forEach((post) => {
+          terminal.pushToStdout(`[${count}] ${post.title}.txt`);
+          count++;
         });
+      },
+    },
+    open: {
+      description: "Open a post by number.",
+      usage: "open <int>",
+      fn: (postNum) => {
+        let { posts } = this.state;
 
-        return "waiting for results";
+        if (!postNum || isNaN(postNum)) return "Please enter a number.";
+        if (Number.parseInt(postNum) >= posts.length)
+          return "Please enter a valid number.";
+
+        const post = posts[Number.parseInt(postNum)];
+        const html = draftToHtml(JSON.parse(post.description));
+        return html;
       },
     },
   };
@@ -39,6 +63,7 @@ class BlogTerminal extends Component {
         commands={this.commands}
         welcomeMessage={"Welcome to the React terminal!"}
         promptLabel={"user@adriensblog:~$"}
+        dangerMode={true}
       />
     );
   }
